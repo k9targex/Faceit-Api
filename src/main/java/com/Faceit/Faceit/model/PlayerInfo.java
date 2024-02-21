8a1f46b040a68ab77d42b374058667d64040c22d
k9targex <dimamozheiko13@gmail.com>
2024-02-21 22:25:24 +0300

    Front

---
+++ src/main/java/com/Faceit/Faceit/model/PlayerInfo.java
@@ -0,0 +1,19 @@
+package com.Faceit.Faceit.model;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PlayerInfo {
+    @JsonProperty("items")
+    private ArrayList<PlayerItem> items;
+}
