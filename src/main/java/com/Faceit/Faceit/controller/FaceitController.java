226690d6aa1cb72d1f888b205a8b6805194ca498
k9targex <dimamozheiko13@gmail.com>
2024-02-23 11:49:29 +0300

    Front

--- src/main/java/com/Faceit/Faceit/controller/FaceitController.java
+++ src/main/java/com/Faceit/Faceit/controller/FaceitController.java
@@ -4,21 +4,33 @@ package com.Faceit.Faceit.controller;
 import com.Faceit.Faceit.model.PlayerInfoAndStats;
 import com.Faceit.Faceit.model.PlayerStats;
 import com.Faceit.Faceit.service.FaceitService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
 
-@RestController
+
+@Controller
 @RequestMapping("api/v1")
 public class FaceitController {
     @GetMapping("/faceit/info")
-    public PlayerInfoAndStats hello(@RequestParam(defaultValue = "s1mple") String nickname) {
-        return FaceitService.getRequest(nickname);
+    public String hello(@RequestParam(defaultValue = "s1mple") String nickname,Model model) {
+        PlayerInfoAndStats playerInfoAndStats = FaceitService.getRequest(nickname);
+        model.addAttribute("playerInfoAndStats",playerInfoAndStats);
+        return "index";
 
     }
 
 
-
+/*
+@GetMapping("/weather")
+        public String getWeather(@RequestParam String word, Model model) {
+        WeatherForecast weatherForecast=weatherService.getWeatherByCity(word);
+        model.addAttribute("weatherForecast",weatherForecast);
+        return "index";
+  }
+ */
 }
